4c8ca210016405f5690068599a880274422f2a1f
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-06-23 16:51:22 +0300

    added props color to Button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import Button from "./components/Button";
 function App() {
   return (
     <>
-      <Button>Primary button</Button>
-      <Button size="large">Primary large button</Button>
-      <Button size="small">Secondary button</Button>
-      <Button disabled size="small">
+      <Button color="primary">Primary button</Button>
+      <Button color="primary" size="large">
+        Primary large button
+      </Button>
+      <Button color="primary" size="small">
+        Secondary button
+      </Button>
+      <Button color="primary" disabled size="small">
         Disabled button
+      </Button>
+      <Button color="secondary">Secondary button</Button>
+      <Button color="secondary" disabled>
+        Secondary button
       </Button>
     </>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -15,28 +15,33 @@ import styled from "styled-components";
 //   }
 // }};
 const StyledButton = styled.button`
-  background-color: #605dec;
-  color: #fafafa;
-  border: none;
+  background-color: ${(props) =>
+    props.color === "primary" ? "#605dec" : "white"};
+  color: ${(props) => (props.color === "primary" ? "#fafafa" : "#605DEC")};
+  border: ${(props) =>
+    props.color === "primary" ? "none" : "1px solid #605DEC"};
   border-radius: 4px;
   margin: 0 8px;
   font-size: ${(props) => (props.size === "large" ? "18px" : "16px")};
   padding: ${(props) => (props.size === "small" ? "8px 16px" : "12px 20px")};
 
   &:hover {
-    background-color: #1513a0;
+    background-color: ${(props) =>
+      props.color === "primary" ? "#1513a0" : "#E9E8FC"};
   }
 
   &:disabled {
-    background: rgba(203, 212, 230, 0.3);
-    outline: 1px solid #7c8db0;
+    background: ${(props) =>
+      props.color === "primary" ? "rgba(203, 212, 230, 0.3)" : "white"};
+    outline: ${(props) =>
+      props.color === "primary" ? "1px solid #7c8db0" : "none"};
     color: #7c8db0;
   }
 `;
 
-const Button = ({ children, size, disabled }) => {
+const Button = ({ children, size, disabled, color }) => {
   return (
-    <StyledButton size={size} disabled={disabled}>
+    <StyledButton size={size} color={color} disabled={disabled}>
       {children}
     </StyledButton>
   );
@@ -46,6 +51,7 @@ Button.propTypes = {
   children: PropTypes.element.isRequired,
   size: PropTypes.oneOf(["small", "medium", "large"]),
   disabled: PropTypes.bool,
+  color: PropTypes.oneOf(["primary", "secondary"]).isRequired,
 };
 
 Button.defaultProps = {
